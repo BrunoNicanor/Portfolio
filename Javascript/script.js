7a748f3ec3c4ed724c2ef2583efcee386d3ff057
BrunoNicanor <88382684+BrunoNicanor@users.noreply.github.com>
2022-11-26 13:48:34 -0300

    Add files via upload

---
+++ Javascript/script.js
@@ -0,0 +1,11 @@
+function pNome(nome){
+    do{
+        nome = prompt("Digite o seu nome: ");
+    }
+    while(nome == null);
+    
+    alert("Obrigado " + nome + " por vizitar minha página :)")
+    // var nN = document.getElementById("principal");
+    // nN.innerHTML = "Obrigado " + nome + " por visitar a página";
+}
+
